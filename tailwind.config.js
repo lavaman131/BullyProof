3d86c0e1f9f5e464ac42013e47cb18c164cdcdcd
Ali Lavaee <alilavaee@Alis-MacBook-Pro.local>
2022-10-30 23:22:36 -0400

    added responsive UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./popup/*.{html, js}",
-    "./options/*.{html, js}"
-  ],
+  content: ["./popup/*.{html, js}", "./options/*.{html, js}"],
   theme: {
     screens: {
       sm: "480px",
       md: "768px",
       lg: "976px",
-      xl: "1440px"
+      xl: "1440px",
     },
     extend: {
       colors: {
         aquaBlue: "rgb(77, 159, 235)",
-        midnightBlue: "rgb(17, 23, 41)"
+        midnightBlue: "rgb(17, 23, 41)",
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
